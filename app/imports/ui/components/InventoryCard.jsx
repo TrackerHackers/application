5c1aee56b493381e1fddcdcb82a62f97effd17ec
chenglisa <97767296+chenglisa@users.noreply.github.com>
2024-01-28 16:35:17 -1000

    Added edit button

--- app/imports/ui/components/InventoryCard.jsx
+++ app/imports/ui/components/InventoryCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 const InventoryCard = ({ stuff }) => (
   <Card style={{ width: '18rem' }} className="h-100 m-auto">
     <Card.Img className="item-img" variant="top" src={stuff.image} style={{ height: '40vh' }} />
-    <Card.Title className="px-3">{stuff.name}</Card.Title>
+    <Card.Title className="px-3 text-center">{stuff.name}</Card.Title>
     <Card.Body>
-      <Card.Text className="mb-auto">{stuff.quantity}</Card.Text>
-      <Card.Text className="mb-auto">{stuff.condition}</Card.Text>
+      <Card.Text className="mb-auto">Quantity: {stuff.quantity}</Card.Text>
+      <Card.Text className="mb-auto">Condition: {stuff.condition}</Card.Text>
+      <Card.Text className="mb-auto d-flex justify-content-center" style={{ marginTop: '10px' }}><a href={`/edit/${stuff._id}`}><Button variant="danger">Edit</Button></a></Card.Text>
     </Card.Body>
   </Card>
 
