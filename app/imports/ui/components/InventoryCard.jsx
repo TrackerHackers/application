fc27b41f23be1c8025540d5969e8ac64d509e0d5
SienneR <rodwells@hawaii.edu>
2024-03-13 19:42:34 -1000

    Card styling

--- app/imports/ui/components/InventoryCard.jsx
+++ app/imports/ui/components/InventoryCard.jsx
@@ -4,8 +4,8 @@ import { Card, Button } from 'react-bootstrap';
 
 const InventoryCard = ({ item }) => (
   <Card style={{ width: '18rem' }} className="h-100 m-auto">
-    <Card.Title className="px-3 text-center">{item.name}</Card.Title>
-    <Card.Subtitle className="card-subtitle mb-2 text-muted" style={{ marginLeft: '10px' }}>Quantity: {item.quantity}</Card.Subtitle>
+    <Card.Title className="px-3 text-center" style={{ marginTop: '10px' }}>{item.name}</Card.Title>
+    <Card.Subtitle className="card-subtitle mb-2 text-muted" style={{ marginLeft: '15px', marginTop: '10px' }}>Quantity: {item.quantity}</Card.Subtitle>
     <Card.Body>
       <Card.Text className="mb-auto">Description: {item.description}</Card.Text>
       <Card.Text className="mb-auto">Condition: {item.rating}</Card.Text>
