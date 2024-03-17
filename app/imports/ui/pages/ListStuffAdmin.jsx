4d03c7e5660d29105b1478c6cae5025f8913e2d7
chenglisa <97767296+chenglisa@users.noreply.github.com>
2024-03-17 12:05:04 -1000

    change stuff -> inventory

--- app/imports/ui/pages/ListStuffAdmin.jsx
+++ app/imports/ui/pages/ListStuffAdmin.jsx
@@ -25,7 +25,7 @@ const ListStuffAdmin = () => {
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col md={7}>
-          <Col className="text-center"><h2>List Stuff (Admin)</h2></Col>
+          <Col className="text-center"><h2>List Inventory (Admin)</h2></Col>
           <Table striped bordered hover>
             <thead>
               <tr>
